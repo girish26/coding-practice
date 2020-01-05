cc48146ef13b0d7f833e3b48e9b77935af7bcb7c
Girish Korde <girikorde26@gmail.com>
2020-01-05 12:26:59 +0530

    egg dropping puzzle

--- src/com/gk/dp/EggDropping.java
+++ src/com/gk/dp/EggDropping.java
@@ -1,8 +1,35 @@
 package com.gk.dp;
 
+import java.util.Arrays;
+
 public class EggDropping {
 
-    public static void main(String[] args) {
+    int solveEggDropping(int floors, int eggs){
+        final int results[][] = new int[floors+1][eggs+1];
+        for(int i=0;i < floors+1; i++)
+                results[i][1] = i;
 
+        for(int i=0;i <eggs+1; i++)
+            results[1][i] = 1;
+
+        for(int i=2; i<floors+1; i++){
+            for(int j=2; j<eggs +1; j++){
+                results[i][j] = Integer.MAX_VALUE;
+                for(int k=1; k<i; k++){
+                    int brokenEggResult = results[k-1][j-1] ;
+                    int survivedEggResult = results[i-k][j];
+                    int temp = Math.max(brokenEggResult, survivedEggResult) +1;
+                    if(temp < results[i][j])
+                        results[i][j] = temp;
+                }
+            }
+        }
+        for (final int[] a : results) {
+            System.out.println(Arrays.toString(a));
+        }
+        return results[floors][eggs];
+    }
+    public static void main(String[] args) {
+        System.out.println(new EggDropping().solveEggDropping(100, 2));
     }
 }
